f7dd5e1b80259c2079d378b4da37b9f1e6c45f5c
kct <lzhkai1111@qq.com>
2015-06-24 09:16:08 +0800

    urls.py

---
+++ autonumber/views.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+ 
+def index(request):
+    return HttpResponse(u"欢迎光临 自强学堂!")
+	
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a)+int(b)
+    return HttpResponse(str(c))	
+
+def add2(request,a,b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+	
+def link(request):
+    tmp = "<a href=\"{% http://127.0.0.1:8000/ 'add2' 4 5 %}\">link</a>"
+    return HttpResponse(tmp)
+	
+def home(request):
+    return render(request, 'home.html')
+	
+	
+	
--- kct/urls.py
+++ kct/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+	
+	url(r'^$', 'autonumber.views.index', name='index'), # http://127.0.0.1:8000/
+	url(r'^add/$', 'autonumber.views.add', name='add'), # http://127.0.0.1:8000/add/?a=4&b=5
+	url(r'^add2/(\d+)/(\d+)/$', 'autonumber.views.add2', name='add2'), # http://127.0.0.1:8000/add2/4/5/
+	url(r'^link/$', 'autonumber.views.link', name='link'), # http://127.0.0.1:8000/link
+	
+	url(r'^home/$', 'autonumber.views.home', name='home'), # http://127.0.0.1:8000/home
 ]
