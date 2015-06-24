07e769dc72bd388796a2caef0e1ba138aaca4d5c
kct317 <lzhkai1111@qq.com>
2015-06-24 21:07:00 +0800

    15062401

--- autonumber/admin.py
+++ autonumber/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Article #models.py的Article
+from .models import Person #models.py的Article
  
- 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title ','pub_date ','update_time ',)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name ',)
+
 admin.site.register(Article)
+admin.site.register(Person)
