8cfa3bcca7ca033e13bbeb68af64e0f9eaab341f
kct317 <lzhkai1111@qq.com>
2015-07-09 20:04:32 +0800

    db table

--- autonumber/models.py
+++ autonumber/models.py
@@ -30,3 +30,38 @@ class Person(models.Model):
 
     def __unicode__(self):
         return self.first_name + self.last_name
+
+
+class Case(models.Model):
+    casename = models.CharField(max_length=128) #案件名称
+
+    litigant = models.CharField(max_length=30) #当事人
+    litiganttype = models.IntegerField() #当事人类型
+
+    caseproperty = models.IntegerField() #案件性质/类型  下拉框
+    casevalue = models.IntegerField() #案值/元
+
+    fines = models.IntegerField() #罚款金额
+    forfeituremoney = models.IntegerField() #没收金额
+
+    forfeitureitem = models.CharField(max_length=128) #没收物品
+    forfeitureamount = models.IntegerField() #没收数量
+
+    illegalfacts = models.TextField() #违法事实
+    law = models.TextField() #违反法律
+    punishbasis = models.TextField() #处罚依据
+
+    createdate = models.DateField() #立案日期
+    informdate = models.DateField() #告知日期
+
+    informnumber = models.CharField(max_length=128) #听证告知书/告知书编号
+    issueddate = models.DateField() #处罚决定书发文日期   *** 处罚决定书的发文日期要大于或等于上一个发文日期
+    decisionnumber = models.CharField(max_length=128)  #行政处罚决定书编号
+    handlingunit = models.CharField(max_length=128) #办案单位
+    auditorman = models.CharField(max_length=30) #核审人员（法制员）
+
+    remarkman = models.TextField() #案件备注
+
+    def __unicode__(self):
+        return self.name
+
