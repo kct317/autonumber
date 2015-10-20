98cc9f221f72e666c2804cbc76e036f92e0ad2f2
kct317 <lzhkai1111@qq.com>
2015-10-20 20:57:50 +0800

    2057

--- app/autonumber/config.py
+++ app/autonumber/config.py
@@ -27,18 +27,18 @@ CONFIG = {
             'id':1,
             'name':'法规所',
             'list':{
-                '开工商告字':'unit=1&type=1',# /id/documenttype
-                '开工商听告字':'unit=1&type=2',
-                '开工商处字':'unit=1&type=3',
+                '开工商告字':'',
+                '开工商听告字':'',
+                '开工商处字':'',
             },
         },
         {
             'id':2,
             'name':'蒸够明',
             'list':{
-                '样衰衰':'unit=2&type=1',# /id/documenttype
-                '懒狗':'unit=2&type=2',
-                '低B':'unit=2&type=3',
+                '样衰衰':'',
+                '懒狗':'',
+                '低B':'',
             },
         },
     ],
